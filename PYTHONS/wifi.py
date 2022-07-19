3b2cae431b476f70930f39b754f7706883b61f13
PRABIN1804 <prabinmishra2004@gmail.com>
2022-07-19 19:48:46 +0530

    files added

--- PYTHONS/wifi.py
+++ PYTHONS/wifi.py
@@ -1,13 +1,13 @@
 import subprocess
 
-data= subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8',errors="backslashreplace").split('\n')
-profiles =[i.split(":"),[1][1:-1] fori in data if "All User profile" in i]
+data = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":"),[1][1:-1] fori in data if "All User profile" in i]
 
-for i in profile:
-    results =subprocess.check_output(['netsh','wlan','show','profile',i,'key=clear']).decode('utf-8',errors="backslashreplace").split('\n')
-    results = [b.spilt(":"),[1][1:-1] for i in results if "key Content" in b]
+for i in profiles:
+    results = subprocess.check_output(['netsh','wlan','show','profile',i,'key=clear']).decode('utf-8').split('\n')
+    results = [ b.spilt(":"),[1][1:-1] for i in results if "key Content" in b ]
     try:
         print ("{:<30}| {:<}".format(i,results[0]))
-        except IndexError:
-            print ("{:<30}| {:<}".format(i,"None"))
-            input("")
+    except IndexError:
+        print ("{:<30}| {:<}".format(i,""))
+  input("")
