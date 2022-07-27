1f42cbf0d14c2fbe5e9b45a9317d012f6628f6ec
PRABIN1804 <prabinmishra2004@gmail.com>
2022-07-27 11:00:17 +0530

    files added

--- DSA/2D_array.c
+++ DSA/2D_array.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-void dispnum(int num[3][3]);
+void dispnum(int num[2][2]);
 int main()
 {
-    int num[3][3];
+    int num[2][2];
     printf("enter the elements of array: \n");
-    for(int i=0; i<3; ++i)
+    for(int i=0; i<2; ++i)
     {
-        for(int j=0;j<3; ++j)
+        for(int j=0;j<2; ++j)
         {
 scanf("%d",&num[i][j]);
         }
@@ -15,12 +15,12 @@ scanf("%d",&num[i][j]);
     return 0;
 }
 
-void dispnum(int num[3][3])
+void dispnum(int num[2][2])
 {
     printf("displaying:\n");
-    for(int i=0; i<3; ++i)
+    for(int i=0; i<2; ++i)
     {
-        for( int j=0; j<3; ++j)
+        for( int j=0; j<2; ++j)
         {
             printf("%d\n",num[i][j]);
         }
