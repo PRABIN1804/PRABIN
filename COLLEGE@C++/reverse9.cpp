177a0f608cfcac01db552605645c739326bf109b
PRABIN1804 <prabinmishra2004@gmail.com>
2022-08-03 22:18:01 +0530

    files added

--- COLLEGE@C++/reverse9.cpp
+++ COLLEGE@C++/reverse9.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int reverse()
 {
     int  a,b,r=0;
-    cout << " enter a nuber to reverse it \n " <<endl;
+    cout << " enter a number to reverse it \n " <<endl;
     cin >> a;
 
     while(a != 0){
@@ -12,6 +12,26 @@ int main()
         a=a/10;
         r=r*10+b;
     }
-    cout << " reversednumber is"<<r <<endl;
+    cout << " reversed number is:"<<r <<endl;
     return 0;
+}
+
+//sum of reverse number
+
+int sum(){
+int n,r,s=0;
+while(n>0){
+    r=n%10;
+    n=n/10;
+    s=s+r;
+}
+cout<<"the sum is:" <<s<<endl;
+;
+}
+int main()
+{
+int n;
+reverse();
+sum();
+return 0;
 }
