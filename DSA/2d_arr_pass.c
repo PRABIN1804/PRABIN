7eff0d8346f465a34ba0a4c51dcd219a4f8952a2
PRABIN1804 <prabinmishra2004@gmail.com>
2022-08-06 09:04:01 +0530

    files added

--- DSA/2d_arr_pass.c
+++ DSA/2d_arr_pass.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
-
-void display(int row, int col, int arr[][col])
+#include<stdio.h>
+void display(int row,int col,int arr[][col])
 {
   for (int i = 0; i < row; i++)
   {
     for (int j = 0; j < col; j++)
     {
-      printf("%d ", arr[i][j]);
+      printf("%d",arr[i][j]);
     }
-    printf("\n");
+      printf("\n");
+    } 
+  };
+  int main()
+  {
+    int arr[][2]={{16, 14},
+                  {12, 10}};
+display(2,2,arr);
+return 0;
   }
-}
-int main()
-{
-  int arr[][2] = {{22, 24},
-                  {12, 16}};
-
-  display(2, 2, arr);
-  return 0;
-}
