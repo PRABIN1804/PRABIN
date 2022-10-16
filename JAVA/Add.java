1ab433da35002c7e9bc77d53d3b1516e03949c45
PRABIN1804 <prabinmishra2004@gmail.com>
2022-10-16 10:42:58 +0530

    files added

---
+++ JAVA/Add.java
@@ -0,0 +1,12 @@
+public class Add 
+{
+    public static void main(String []args)
+    {
+        System.out.println("Enter two numbers");
+        int first = 5;
+        int second = 20;
+        System.out.println(first + " " + second);
+        int sum = first + second;
+        System.out.println("the sum is: " + sum);
+    }
+}
