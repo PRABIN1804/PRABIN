b0259eb6b4723317b43df2bcbb8d4de22eacdb46
PRABIN1804 <prabinmishra2004@gmail.com>
2022-12-12 08:27:24 +0530

    files added

---
+++ JAVA/Even_odd.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Even_odd 
+{
+public static void main(String[] args) {
+System.out.println("Enter a number:");
+Scanner sc = new Scanner(System.in);
+int num = sc.nextInt();
+
+}    
+}
