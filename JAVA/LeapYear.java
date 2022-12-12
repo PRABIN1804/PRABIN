90e09ee82922f4291c5bb77702786e862b5c7064
PRABIN1804 <prabinmishra2004@gmail.com>
2022-12-12 13:17:23 +0530

    files added

--- JAVA/LeapYear.java
+++ JAVA/LeapYear.java
@@ -1,32 +1,20 @@
+import java.util.Scanner;
+
 public class LeapYear {
-    
+  private static boolean isLeapYear (int y) {
+    if ((y%4==0 && y%100!=0)|| y%400==0)
+    return true;
+    else
+    return false; 
+  }public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter a year:");
+    int year=in.nextInt();
 
-        public static void main(String[] args) {
-
-          int year = 2000;
-          boolean leap = false;
-          if (year % 4 == 0) {
-
-            if (year % 100 == 0) {
-
-
-              if (year % 400 == 0)
-                leap = true;
-              else
-                leap = false;
-            }
-
-            else
-              leap = true;
-          }
-          
-          else
-            leap = false;
-      
-          if (leap)
-            System.out.println(year + " is a leap year.");
-          else
-            System.out.println(year + " is not a leap year.");
-        }
-    }
-
+    if (isLeapYear(year))
+      System.out.println("This  is a leap year.");
+    else
+      System.out.println( "This is not a leap year.");
+      in.close();
+  }
+}
