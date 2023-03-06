5ab3f9ce47ee2e52bbc07b06749922a5950406c1
PRABIN1804 <prabinmishra2004@gmail.com>
2023-03-06 09:53:13 +0530

    files added

--- C-PROGRAMS/add.c
+++ C-PROGRAMS/add.c
@@ -1,11 +1,19 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int number1,number2,sum;
+//     printf("enter two number");
+//    scanf("%d %d",&number1,&number2);
+//    // calculating sum
+//    sum = number1 + number2;
+//    printf("%d + %d = %d", number1,number2,sum);
+//    return 0;
+// }
+
+
 #include<stdio.h>
 int main()
 {
-    int number1,number2,sum;
-    printf("enter two number");
-   scanf("%d %d",&number1,&number2);
-   // calculating sum
-   sum = number1 + number2;
-   printf("%d + %d = %d", number1,number2,sum);
-   return 0;
+    int a=10,b=20,c=30;
+    printf("%d",a+=(a+=3,5,a));
 }
